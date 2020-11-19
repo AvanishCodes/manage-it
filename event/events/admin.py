550d15d1fcf4893e7918c40a53450071551a771c
Avanish Gupta <ui19co15@iiitsurat.ac.in>
2020-11-19 11:26:22 +0530

    deleted the previous project

--- event/events/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import event, person
-
-# Register your models here.
-
-admin.site.register(person)
-admin.site.register(event)
--- event/events/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('', views.index, name='index'),
-    path('', views.events, name='events'),
-    path('<int:eventID>', views.eventDetail, name='eventDetail'), 
-]
